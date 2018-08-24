e4cd470ef1c443bca68334ad146384459918bd1f
MAHMOUDHASSANHAMED <heshamalamam73@gmail.com>
2018-08-24 10:45:15 +0000

    finish backend api

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -1,30 +1,16 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+var helpers = require("../helpers/todos.js");
 
 
-
-router.get('/',function(req, res){
-  db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-router.post('/', function(req, res){
- db.Todo.create(req.body)
- .then(function(newTodo){
-   res.status(201).json(newTodo);
- })
- .catch(function(err){
-   res.send(err);
- });
-});
-
-
-
+router.route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodos);
+router.route('/:todoId')
+  .get(helpers.getTodo)
+  .put(helpers.ubdateTodo)
+  .delete(helpers.deleteTodo);
 
 
 
