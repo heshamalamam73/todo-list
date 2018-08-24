a10a8da42801492f4b7c7971988d4dabfa017e86
MAHMOUDHASSANHAMED <heshamalamam73@gmail.com>
2018-08-24 18:00:16 +0000

    add remove from input

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -2,12 +2,20 @@
 
 
 $(document).ready(function(){
+    
+    $('#todoInput').keypress(function(event){
+        if(event.which== 13 ){
+            createTodo();
+        }
+    });
     $.getJSON('/api/todos')
-    .then(addTodos)
+    .then(addTodos);
         
     });
     
-    
+$('.list').on('click','span',function(){
+    removeTodo($(this).parent());
+});
     
     
     
@@ -33,7 +41,46 @@ $(document).ready(function(){
 
 function addTodos(todos){
     todos.forEach(function(todo){
-    var newTodo = $('<li class ="task" >' + todo.name + '</li>');
+        addTodo(todo);
+   });
+}
+
+function addTodo(todo){
+     var newTodo = $('<li class ="task" >' + todo.name + '<span>x</span>' + '</li>');
+     newTodo.data('id',todo._id);
+    if(todo.completed){
+    newTodo.addClass('done');
+    }
       $('.list').append(newTodo);
+    }
+    
+
+function createTodo(){
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos',{name: usrInput})
+    .then(function(newTodo){
+      addTodo(newTodo);
+      
+      $('#todoInput').val('');
+    })
+    .catch(function(err){
+        console.log(err);
     });
 }
+
+function removeTodo(todo){
+var idClicked =todo.data('id') ;
+ var idDeleted ='/api/todos/' + idClicked;
+ $.ajax({
+     method : 'DELETE',
+     url : idDeleted
+ })
+
+ .then(function(data){
+     todo.remove();
+ });
+}
+    
+
+
+
